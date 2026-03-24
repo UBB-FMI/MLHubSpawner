0a768d5553b4cefb1a0b00f718753bc83c78277a
KiralyCraft <alexucu@gmail.com>
2026-03-24 12:02:10 +0200

    Resource loading and node status updates

--- mlhubspawner/form_builder.py
+++ mlhubspawner/form_builder.py
@@ -1,30 +1,14 @@
 import json
 import os
 
-try:
-    import pkg_resources
-except ImportError:
-    pkg_resources = None
-
 class JupyterFormBuilder():
     def __init__(self):
-        # Read the default template for the form
-        if pkg_resources is not None:
-            try:
-                resource_path = pkg_resources.resource_filename('mlhubspawner', 'resources/form.html')
-                with open(resource_path, 'r') as file:
-                    self.form_html_content = file.read()
-            except Exception as e:
-                self.form_html_content = f"FORM_TEMPLATE_ERROR: {e}"
-        else:
-            # When pkg_resources is not available, assume that the 'resources' folder
-            # is in the same directory as this file.
-            try:
-                resource_path = os.path.join(os.path.dirname(__file__), 'resources', 'form.html')
-                with open(resource_path, 'r') as file:
-                    self.form_html_content = file.read()
-            except Exception as e:
-                self.form_html_content = f"FORM_TEMPLATE_ERROR (local): {e}"
+        try:
+            resource_path = os.path.join(os.path.dirname(__file__), 'resources', 'form.html')
+            with open(resource_path, 'r') as file:
+                self.form_html_content = file.read()
+        except Exception as e:
+            self.form_html_content = f"FORM_TEMPLATE_ERROR: {e}"
 
     def _safe_fetch(self, formdata, key, default):
         return formdata[key][0] if key in formdata else default
